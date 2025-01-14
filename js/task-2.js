ec333c77f67259071988b34be2734460104322ac
kvazar <marina810920@gmail.com>
2025-01-14 23:45:18 +0200

    tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,37 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
+class Storage {
+	#items;
+	
+	constructor(elem) {
+		this.#items = elem;
+	}
+	
+	getItems() {
+		return this.#items;
+	}
+	
+	addItem(newItem) {
+		this.#items.push(newItem);
+	}
+	
+	removeItem(itemToRemove) {
+		const itemsNew = [];
+		for(const elem of this.#items) {
+			if(elem !== itemToRemove) {
+				itemsNew.push(elem);
+			}
+		}
+		this.#items = itemsNew;
+	}
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
